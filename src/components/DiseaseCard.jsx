5954d4da9312154f72044e0af220fae163a49059
vimalds15 <antovimalands@gmail.com>
2023-03-05 16:11:44 +0530

    added disease component and list

---
+++ src/components/DiseaseCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DiseaseCard = () => {
+  return (
+    <div>DiseaseCard</div>
+  )
+}
+
+export default DiseaseCard
---
+++ src/diseases/diseases.js
@@ -0,0 +1,17 @@
+export const diseases = [
+    {
+        name:"MESOTHELIOMA"
+    },
+    {
+        name:"LIVER CIRRHOSIS"
+    },
+    {
+        name:"CHRONIC KIDNEY DISEASE"
+    },
+    {
+        name:"CORONARY HEART DISEASE"
+    },
+    {
+        name:"DIABETES MELLITUS"
+    }
+]

61fd3630e3103cdf2969dde7a5e94b43387b8e98
vimalds15 <antovimalands@gmail.com>
2023-03-05 19:51:14 +0530

    added navigation

--- src/components/DiseaseCard.jsx
+++ src/components/DiseaseCard.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const DiseaseCard = () => {
+const DiseaseCard = ({name,id}) => {
   return (
-    <div>DiseaseCard</div>
+    <Link to={`detail/${id}`} className='flex items-center justify-center w-full h-full'>
+    <div className='flex items-center justify-center w-[90%] lg:w-[50%] max-w-xl bg-white py-7 mb-10 rounded shadow-xl hover:shadow-2xl transition-all cursor-pointer'>
+        <p className='font-medium bg-red'>{name}</p>
+    </div>
+    </Link>
   )
 }
 

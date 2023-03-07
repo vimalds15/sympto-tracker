7f4f02c014b189e8887c1e19d7daddef09713ce6
vimalds15 <antovimalands@gmail.com>
2023-03-07 19:48:13 +0530

    added LiverCirrhosisPage

---
+++ src/pages/LiverCirrhosisPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LiverCirrhosis from '../components/LiverCirrhosis.Form'
+
+const LiverCirrhosisPage = () => {
+  return (
+    <div className='flex items-center justify-center'>
+        <LiverCirrhosis />
+    </div>
+  )
+}
+
+export default LiverCirrhosisPage

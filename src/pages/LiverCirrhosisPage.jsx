d698a03a397e160d153b5c716123ca4470c1e246
vimalds15 <antovimalands@gmail.com>
2023-03-07 19:41:02 +0530

    some changes

--- src/pages/LiverCirrhosisPage.jsx
+++ src/pages/LiverCirrhosisPage.jsx
@@ -5,7 +5,7 @@ import LiverCirrhosis from '../components/LiverCirrhosis.Form'
 const LiverCirrhosisPage = () => {
   return (
     <div className='flex items-center justify-center'>
-    <LiverCirrhosis />
+    <LiverCirrhosis  />
     </div>
   )
 }

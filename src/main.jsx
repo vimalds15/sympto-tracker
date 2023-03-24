d1e30f3d13464b1014b0e66b98ae6740508a20d7
vimalds15 <antovimalands@gmail.com>
2023-03-24 22:07:12 +0530

    configured axios defaults

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import axios from "axios"
+
+axios.defaults.baseURL="http://127.0.0.1:5000/"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

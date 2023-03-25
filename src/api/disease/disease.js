981e601d0459371b8ef4092803fa00f1e9ba884a
vimalds15 <antovimalands@gmail.com>
2023-03-25 09:43:39 +0530

    added some more apis

--- src/api/disease/disease.js
+++ src/api/disease/disease.js
@@ -4,7 +4,19 @@ const DiseaseService = {
     predictMesothelioma: async (formData) => {
             const {prediction} = await ApiService.predictDisease("mesothelioma",formData)
             return prediction
-    }
+    },
+    predictChronicKidneyDisease: async(formData)=>{
+        const {prediction} = await ApiService.predictDisease("chronickidneydisease",formData)
+        return prediction
+    },
+    predictCoronaryHeartDisease: async(formData)=>{
+        const {prediction} = await ApiService.predictDisease("coronaryheartdisease",formData)
+        return prediction
+    },
+    predictDiabetesMelitus: async(formData)=>{
+        const {prediction} = await ApiService.predictDisease("diabetesmelitus",formData)
+        return prediction
+    },
 }
 
 export default DiseaseService

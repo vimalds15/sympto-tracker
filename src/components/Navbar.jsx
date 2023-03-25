41e62f18bc10cbb34198f10b9b4462e235f5d3fe
vimalds15 <antovimalands@gmail.com>
2023-03-25 10:53:23 +0530

    added link to homescreen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <>
     <div className='flex items-center justify-center h-14  shadow-xl '>
+      <Link to='/'>
         <p className='font-bold text-gray-600'>Sympto Tracker</p>    
+        </Link>
     </div>   
     </>
   )

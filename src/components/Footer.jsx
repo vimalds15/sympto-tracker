50dcadc18dceb2b5a8c136d00804e50c6d6cb417
vimalds15 <antovimalands@gmail.com>
2023-03-25 10:52:24 +0530

    fixed footer and added montserrat google font

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,9 @@ import aniketLogo from "../assets/ak.svg"
 
 const Footer = () => {
   return (
-    <div className='flex bg-black	 w-full h-52 justify-center'>
-        <img src={vimalLogo} className="h-52" />
+    <div className='flex bg-black	 w-full h-52 justify-center items-center'>
+        {/* <img src={vimalLogo} className="h-52" /> */}
+        <p className='text-gray-300 text-sm'>&copy; Sympto Tracker 2023.</p>
     </div>
   )
 }

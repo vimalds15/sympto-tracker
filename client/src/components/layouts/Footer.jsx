cc7bd21e3b57b5a3ea9f8856320db13348b3336c
vimalds15 <antovimalands@gmail.com>
2023-03-25 23:15:43 +0530

    reduced footer height

--- client/src/components/layouts/Footer.jsx
+++ client/src/components/layouts/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='flex bg-black	 w-full h-52 justify-center items-center'>
+    <div className='flex bg-black	 w-full h-32 justify-center items-center'>
         <p className='text-gray-300 text-sm'>&copy; Sympto Tracker 2023.</p>
     </div>
   )
